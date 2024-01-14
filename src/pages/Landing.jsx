1495e16491be056beab46b67edba32ab3af1ace5
georgeowusu <gkojunior@gmail.com>
2024-01-13 19:10:14 -0500

    set up router

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,13 +1,13 @@
-import logo from '../assets/images/logo.svg'
+import { Link } from 'react-router-dom'
 import main from '../assets/images/main.svg'
-import styled from 'styled-components'
+import Wrapper from '../assets/wrappers/LandingPage'
 
 const Landing = () => {
 	return (
 		<Wrapper>
 			<nav>
-				<img src={logo} alt='Jobster logo' className='logo' />
-				{/* <h1>job hunt</h1> */}
+				{/* <img src={logo} alt='Jobster logo' className='logo' /> */}
+				<h3>job hunt</h3>
 			</nav>
 			<div className='container page'>
 				<div className='info'>
@@ -15,9 +15,13 @@ const Landing = () => {
 						job <span>tracking</span> app
 					</h1>
 					<p>
-						Job Hunt will allow you to track the progress of your interviews. It allows you to stay focus and organized, while keeping all interview up to date .
+						Job Hunt will allow you to track the progress of your interviews. It
+						allows you to stay focus and organized, while keeping all interview
+						up to date .
 					</p>
-					<button className='btn btn-hero'>Login/Register</button>
+					<Link to='register' className='btn btn-hero'>
+						Login/Register
+					</Link>
 				</div>
 				<img src={main} alt='job hunt' className='img main-img' />
 			</div>
@@ -26,41 +30,3 @@ const Landing = () => {
 }
 
 export default Landing
-
-const Wrapper = styled.main`
-	nav {
-		width: var(--fluid-width);
-		max-width: var(--max-width);
-		margin: 0 auto;
-		height: var(--nav-height);
-		display: flex;
-		align-items: center;
-	}
-	.page {
-		min-height: calc(100vh - var(--nav-height));
-		display: grid;
-		align-items: center;
-		margin-top: -3rem;
-	}
-	h1 {
-		font-weight: 700;
-		span {
-			color: var(--primary-500);
-		}
-	}
-	p {
-		color: var(--grey-600);
-	}
-	.main-img {
-		display: none;
-	}
-	@media (min-width: 992px) {
-		.page {
-			grid-template-columns: 1fr 1fr;
-			column-gap: 3rem;
-		}
-		.main-img {
-			display: block;
-		}
-	}
-`
